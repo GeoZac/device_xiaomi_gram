907688f3f590b231fe9912909b5503ecea3b4b86
Rahul Shahare <rshaha@codeaurora.org>
2020-12-07 21:31:10 +0530

    gram: Add kpti=off to boot command line
    This will OFF kernel page table isolation.
    
    Change-Id: Idd36c359d4955701e9ee3ee27a58177a8d622c86
    Signed-off-by: Rahul Shahare <rshaha@codeaurora.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,6 +64,7 @@ BOARD_KERNEL_CMDLINE += androidboot.memcg=1
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
 BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0xa88000
+BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += msm_rtb.enabled=1
