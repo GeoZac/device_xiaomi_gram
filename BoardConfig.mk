9fb55f60b50ac20123d1b5aa81bc95c172d35ed7
George Zacharia <george.zcharia@gmail.com>
2021-09-19 21:07:55 +0530

    gram: we're scanning for eligible devices now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,9 +135,6 @@ TARGET_SCREEN_DENSITY := 440
 include device/qcom/sepolicy/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/dev/input/event3"
-
 # Treble
 BOARD_VNDK_VERSION := current
 
