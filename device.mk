1b4e62cd10eb82a6632ac91a420a74093b7d37d6
George Zacharia <george.zcharia@gmail.com>
2021-10-16 20:30:18 +0530

    gram: Provide fstab as in stock
    Also kernel commadnline has `androidboot.fstab_suffix=default`

--- device.mk
+++ device.mk
@@ -60,7 +60,8 @@ PRODUCT_PACKAGES += \
 
 # fstab
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.default \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.emmc
 
 # FM
 PRODUCT_PACKAGES += \
