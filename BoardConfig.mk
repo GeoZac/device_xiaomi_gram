e99b96d7d7d31aa7a0c218256b00d5fddcf25c03
George Zacharia <george.zcharia@gmail.com>
2024-01-24 23:15:18 +0530

    [TEMP] Switch kernel config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,7 @@ KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 # TARGET_KERNEL_CLANG_VERSION := latest
-TARGET_KERNEL_CONFIG := cust_defconfig
+TARGET_KERNEL_CONFIG := vendor/xiaomi/miatoll_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
